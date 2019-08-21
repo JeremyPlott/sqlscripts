0b6041718df8c5f5065dd24e15aba39c9e996f05
Jeremy Plott <jeremyplott@gmail.com>
2019-08-21 12:18:30 -0400

    Added some parameterized queries

--- writingfunction.sql
+++ writingfunction.sql
@@ -1,18 +1,26 @@
+-------------------------------------------ASSIGN PARAMETERS---------------------------------------
+
 declare @LowSAT int;
 declare @HighSAT int;
+declare @LowGPA decimal(4,2);
+
 
 set @LowSAT  = 1000;
 set @HighSAT = 1400;
+set @LowGPA  = 2.7;
 
+-------------------------------------------------CODE----------------------------------------------
 
 select 
-	*
+	--*
+	avg(SAT) 'Avg SAT'
 
 	from student
 
 		--where student.SAT >= 1000 and student.SAT <= 1300
-		where student.SAT <  @LowSAT or  student.SAT >  @HighSAT
-
+		--where student.SAT <  @LowSAT or student.SAT > @HighSAT
+		--where student.GPA >= @LowGPA
+		
 
 	
-	order by student.SAT desc, student.lastname
+	--order by student.SAT desc, student.lastname
