8f82144dc8bf97641d4655ee8bfcc43094e97b55
Jeremy Plott <jeremyplott@gmail.com>
2019-08-22 10:07:27 -0400

    initial commit

---
+++ transactions.sql
@@ -0,0 +1,25 @@
+begin transaction
+
+	insert into Student (id, Firstname, Lastname, SAT, GPA, MajorId)
+				values (999, 'Jane', 'Doe', 2400, 2.1, null)
+
+							if @@ROWCOUNT <> 1 
+							BEGIN
+								rollback transaction
+							END
+
+	insert into Student (id, Firstname, Lastname, SAT, GPA, MajorId)
+				values (998, 'John', 'Smith', 600, 4.0, 999)
+
+							if @@ROWCOUNT <> 1 
+							BEGIN
+								rollback transaction
+							END
+
+commit transaction
+
+/*
+select * from student where id in (998, 999)
+
+delete from student where id = 998
+*/
